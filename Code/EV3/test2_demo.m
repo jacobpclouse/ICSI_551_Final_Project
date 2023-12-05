f497076e788c41c17dae153c29e06e19a1f59529
jc366512 <jclouse@albany.edu>
2023-12-05 11:39:49 -0500

    working on the motor test!

--- Code/EV3/test2_demo.m
+++ Code/EV3/test2_demo.m
@@ -25,22 +25,27 @@
 %
 % ENTER MODIFICATION INFORMATION HERE
 
+clear all; % make sure to remove any pre existing variables before instantiation
 disp('Starting Demo 2: Motor Test');
 
+% Connect to the EV3 brick
+myev3 = legoev3('USB'); % Use 'USB' as the connection type
+% myev3 = legoev3('Bluetooth', 'EV3'); % using bluetooth to connect -- could not get to connect
 
-% Connect to the EV3 brick - 'BTHUB0'
-myLegoEV3 = legoev3('USB'); % Use 'USB' as the connection type
-% myLegoEV3 = legoev3('Bluetooth', 'EV3'); % using bluetooth to connect, replace EV3 with the name of your brick
-% displayText(myLegoEV3, 'EV3', 'FontSize', 20);
+% make starting beep
+playTone(myev3, 500, 0.5);
+writeLCD(myev3,'Demo 2: Motor Test') % display text on the ev3 display
+
 
 % Move the motor connected to port 'A' for 2 seconds
-motorA = motor(myLegoEV3, 'A');
-start(motorA, 20);
-pause(2);
-stop(motorA);
+%motorA = motor(myev3, 'A');
+%start(motorA, 20);
+%pause(2);
+%stop(motorA);
 
-% make beep
-playTone(myLegoEV3, 500, 0.5);
+% make ending beep
+playTone(myev3, 600, .5);
+%clearLCD(myev3) % clear text from display
 
 % Disconnect EV3
-disconnect(myLegoEV3);
+%disconnect(myev3);
